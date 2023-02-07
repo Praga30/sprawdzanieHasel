fba4d51f1d4901e95c6bdffe06d10676b6df946b
Marcin Pranga <mpraga30@gmail.com>
2023-02-07 15:18:45 +0100

    Dodanie zmiennych + sprawdzanie tekstu w hasle

--- app/src/main/java/com/example/sprawdzaniehasla/MainActivity.kt
+++ app/src/main/java/com/example/sprawdzaniehasla/MainActivity.kt
@@ -4,23 +4,46 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.CheckBox
+import android.widget.EditText
+import android.widget.TextView
 import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val sprawdzeniehas=findViewById<CheckBox>(R.id.Zgodne)
+
+        val Wprowadzanie = findViewById<EditText>(R.id.Wprowadzanie)
+        val Potwierdzanie = findViewById<EditText>(R.id.Potwierdzanie)
+        val Zgodnosc =findViewById<CheckBox>(R.id.Zgodne)
         val WielkaLitera = findViewById<CheckBox>(R.id.wielkalit)
         val MalaLitera = findViewById<CheckBox>(R.id.malalit)
         val Cyfra = findViewById<CheckBox>(R.id.Cyfr)
         val ZnakSpec = findViewById<CheckBox>(R.id.znakspec)
         findViewById<Button>(R.id.Sprawdz).setOnClickListener {
-            var haslo=findViewById<TextInputEditText>(R.id.Wprowadzanie).toString()
-            var potwierdzeniehasla=findViewById<TextInputEditText>(R.id.Potwierdzanie).toString()
+            var haslo = Wprowadzanie.text.toString()
+            var potwierdzeniehasla = Potwierdzanie.text.toString()
+            if(haslo.isEmpty() && potwierdzeniehasla.isEmpty())
+            {
+                findViewById<EditText>(R.id.Wprowadzanie).setText("Pole nie moze byc puste")
+            }else{
+                if (haslo==potwierdzeniehasla) {
+                    Zgodnosc.isChecked=haslo.any()
+                }
+                if (haslo.any { it.isUpperCase() } && potwierdzeniehasla.any { it.isUpperCase() }) {
+                    WielkaLitera.isChecked = haslo.any()
+                }
+                if (haslo.any { it.isLowerCase() } && potwierdzeniehasla.any { it.isLowerCase() }) {
+                    MalaLitera.isChecked = haslo.any()
+                }
+                if (haslo.any { it.isDigit() } && potwierdzeniehasla.any { it.isDigit() }) {
+                    Cyfra.isChecked = haslo.any()
+                }
+                if (haslo.any { !it.isLetterOrDigit() } && potwierdzeniehasla.any { !it.isLetterOrDigit() }) {
+                    ZnakSpec.isChecked = haslo.any()
+                }
 
-
-
+            }
         }
     }
 }
